506c86951aaad6f3c185d5aa8864d815da86e103
rgrsolucoes <rgr@rgrsolucoes.com.br>
2023-08-24 21:00:09 -0300

    2408232100

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,9 +58,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8ffef1ad59b68a4bc216aa49b5340168",
-"/": "8ffef1ad59b68a4bc216aa49b5340168",
-"main.dart.js": "dfd6ff711831ec14858feb4d9fb3ec0d",
+"index.html": "40a80613db7761a41541881d22ccb42e",
+"/": "40a80613db7761a41541881d22ccb42e",
+"main.dart.js": "7ec96137fa23d39bae864aab8798c129",
 "manifest.json": "0675cbfb77b594da43017aa777246335",
 "version.json": "7d757bb15399951b5b011d8370b95ac7"
 };
