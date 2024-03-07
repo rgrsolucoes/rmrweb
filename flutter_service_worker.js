1c5b691625d97c1884bddc937e351dcda55b4172
rgrsolucoes <rgr@rgrsolucoes.com.br>
2024-03-07 17:47:22 -0300

    070324

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,9 +65,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b543c59b63f39df8f457acb987b37444",
-"/": "b543c59b63f39df8f457acb987b37444",
-"main.dart.js": "e4aaded48e8d4721388a87c9e934581a",
+"index.html": "12fa493a4d36f421c05f72876b80ff81",
+"/": "12fa493a4d36f421c05f72876b80ff81",
+"main.dart.js": "e4e9749ecd5b2dc9424ebdebf3246b8c",
 "manifest.json": "0675cbfb77b594da43017aa777246335",
 "version.json": "7d757bb15399951b5b011d8370b95ac7"
 };
